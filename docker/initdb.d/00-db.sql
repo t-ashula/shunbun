03d682c994e7bc8e710df858e5d7591ecf98b763
t-ashula <t.ashula+github@gmail.com>
2024-05-28 23:00:35 +0900

    :factory: Add db by docker

---
+++ docker/initdb.d/00-db.sql
@@ -0,0 +1,4 @@
+create database if not exists shunbun character set 'utf8mb4' collate 'utf8mb4_bin';
+
+create database if not exists shunbun_shadow character set 'utf8mb4' collate 'utf8mb4_bin';
+
---
+++ docker/initdb.d/01-user.sql
@@ -0,0 +1,4 @@
+create user if not exists shunbun identified by 'shunbun1' ;
+
+grant all on shunbun.* to 'shunbun';
+grant all on shunbun_shadow.* to 'shunbun';
